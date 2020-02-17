297d2e0aeccb037e1b44e22239cb841a6a53d452
root <root@ip-172-31-11-106.us-east-2.compute.internal>
2020-02-17 07:45:16 +0000

    second commit

---
+++ calc.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    """Add Function"""
+    return x + y
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+def multiply(x,y):
+    """Multiply Function"""
+    return x * y
+
+
